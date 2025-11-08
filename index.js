e5f33eef7afc4e89999ea9cb081dff750aa4d8d1
Rjack00 <Rjackson77@comcast.net>
2025-11-07 18:29:16 -0700

    index.js updated

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose')
 
 app.use(cors())
 app.use(express.static('public'))
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+
+
+
+
 app.get('/', (req, res) => {
+  console.log('DB URL: ', process.env.MONGO_URI)
   res.sendFile(__dirname + '/views/index.html')
 });
 
