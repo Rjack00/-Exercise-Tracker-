0d862e023f7c8eadadb06d8e9e7f3c61ad8a32c2
Rjack00 <Rjackson77@comcast.net>
2025-11-11 18:36:57 -0700

    index.js updated

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 app.use(cors())
+
+
+// Parse URL-encoded bodies (for form data)
+// This is essential for POST requests with form data
+// changes urlencoded data (username=john&description=running&duration=30) to js object
+app.use(express.urlencoded({ extended: false}))
+
+app.use(express.json())
+
 app.use(express.static('public'))
 
 mongoose.connect(process.env.MONGO_URI)
@@ -13,12 +22,22 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 
-
-
 app.get('/', (req, res) => {
-  console.log('DB URL: ', process.env.MONGO_URI)
   res.sendFile(__dirname + '/views/index.html')
 });
+
+const exerciseSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, default: Date.now }
+});
+
+const userSchema = new mongoose.Schema({
+  userName: { type: String, required: true, unique: true },
+  log: [exerciseSchema]
+});
+
+module.exports = mongoose.model('User', userSchema);
 
 
 
