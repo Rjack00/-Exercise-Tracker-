b1d1dd314cbbdeaa19c545712ebf5e766ab65a7a
Rjack00 <Rjackson77@comcast.net>
2026-06-09 18:59:16 -0600

    user.js updated
    added trim and maxlength to schema

--- models/user.js
+++ models/user.js
@@ -1,13 +1,30 @@
 const mongoose = require('mongoose')
 
 const exerciseSchema = new mongoose.Schema({
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  date: { type: Date, default: Date.now }
+  description: { 
+    type: String, 
+    required: true,
+    trim: true,
+    maxlength: 100
+  },
+  duration: { 
+    type: Number, 
+    required: true 
+  },
+  date: { 
+    type: Date, 
+    default: Date.now 
+  }
 });
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  username: { 
+    type: String, 
+    required: true, 
+    unique: true,
+    trim: true,
+    maxlength: 30 
+  },
   log: [exerciseSchema]
 });
 
